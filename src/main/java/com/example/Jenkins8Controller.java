7c79a59ed07dea34f45f9b8ddbc438abb1b3ec55
Saika <sailakshmidamacharla05@gmail.com>
2025-04-21 23:06:06 -0500

    Second commit

--- src/main/java/com/example/Jenkins8Controller.java
+++ src/main/java/com/example/Jenkins8Controller.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Jenkins8Controller {
 	
 @GetMapping("/Sample2")
-public String method1() {
+public String method1(HttpServletRequest req) {
 	return "Hello World using Java 8!!";
 }
 }
